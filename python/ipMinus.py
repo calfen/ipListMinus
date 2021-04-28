1d3c57f98312c3f79235a9737ac45e8d58be83a7
calfen <calfen@gmail.com>
2021-04-28 23:35:50 +0800

    格式化

--- python/ipMinus.py
+++ python/ipMinus.py
@@ -1,27 +1,32 @@
 import xlrd
 from IpList import IpList
-from django.template.defaultfilters import center
-def readExcelSingle(fileName,colNum,rowNum):
+
+
+def readExcelSingle(fileName, colNum, rowNum):
     '''
     读取1行Excel数据
     parameter: fileName 文件名
                 colNum  从第几列读取
                 rowNum  从第几行读取
     返回ip地址List
-    
+
     '''
-    workbook = xlrd.open_workbook(fileName)
+    try:
+        workbook = xlrd.open_workbook(fileName)
+    except Exception:
+        print('文件找不到')
     sheet1 = workbook.sheet_by_index(0)
     ipAddress = []
-    for rowIndex in range(rowNum - 1,sheet1.nrows):
-        colIP = sheet1.cell_value(rowIndex,colNum - 1)
+    for rowIndex in range(rowNum - 1, sheet1.nrows):
+        colIP = sheet1.cell_value(rowIndex, colNum - 1)
         if colIP != '':
             ipAddress.append(colIP)
     return ipAddress
 
-fileName = 'test.xlsx'
-ipStringListCenter = readExcelSingle(fileName,1,3)
-ipStringListBranch = readExcelSingle(fileName,3,3)
+
+fileName = '../test.xlsx'
+ipStringListCenter = readExcelSingle(fileName, 1, 3)
+ipStringListBranch = readExcelSingle(fileName, 3, 3)
 ipObjListCenter = []
 ipObjListBranch = []
 for ipString in ipStringListCenter:
@@ -32,7 +37,7 @@ for ipString in ipStringListBranch:
     ipObj = IpList()
     ipObj.setByString(ipString)
     ipObjListBranch.append(ipObj)
-indexCenter = 0
+    indexCenter = 0
 for ipObjCenter in ipObjListCenter:
     lineNum = []
     indexBranch = 0
@@ -49,27 +54,23 @@ for ipObjCenter in ipObjListCenter:
 #     print(lineNum)
     if len(lineNum) > 0:
         newIpObj = IpList()
-        newIpObj = ipObjCenter - conflictIpObj        
-        print('总部的第' + str(indexCenter + 3) + '行: ' + ipStringListCenter[indexCenter] + 
-                    ' 和:')
+        newIpObj = ipObjCenter - conflictIpObj
+        print('总部的第' + str(indexCenter + 3) + '行: ' +
+              ipStringListCenter[indexCenter] + ' 和:')
         for i in lineNum:
-            print ('分支第' +str(i + 3) + '行: 冲突' + ipStringListBranch[i] )
+            print('分支第' + str(i + 3) + '行: 冲突' + ipStringListBranch[i])
         if newIpObj.ipList == []:
             print('应删除')
         else:
             print('应改为:')
-            newIpObj.printCommonFormat() 
-            print('或改为:' )
+            newIpObj.printCommonFormat()
+            print('或改为:')
             newIpObj.printStartEndFormat()
-        
-    indexCenter += 1
-            
 
-            
+    indexCenter += 1
+
 
 # [print(ipObj.ipList) for ipObj in ipObjListCenter]
 # [print(ipObj.ipList) for ipObj in ipObjListBranch]
-# centerIpObjList = [IpList().setByString(ipString) for ipString in ipStringListCenter]
 # print(centerIpObjList)
 # [print(ipObj.ipList) for ipObj in centerIpObjList]
-    
